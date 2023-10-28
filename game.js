3b8c4d784a528c8e4608c2dd673000f8869c3d24
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2023-10-28 11:49:44 -0400

    document.querySelector bottom left

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
 const topLeft = document.querySelector('.top-left');
 const topRight = document.querySelector('.top-right');
+const bottomLeft = document.querySelector('.bottom-left');
 const bottomRight = document.querySelector('.bottom-right');
