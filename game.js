5bb4b7c942ca55efab103d6743251715646095df
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2023-10-29 11:35:14 -0400

    startButton

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+const startButton = document.querySelector('.start');
 const topLeft = document.querySelector('.top-left');
 const topRight = document.querySelector('.top-right');
 const bottomLeft = document.querySelector('.bottom-left');
