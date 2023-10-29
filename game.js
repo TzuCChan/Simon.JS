80c58ee91bbac19a75d1845be9aef5cdc43db91f
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2023-10-29 11:39:44 -0400

    roundCounter

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 const startButton = document.querySelector('.start');
 const restartButton = document.querySelector('.restart');
+const counter = document.querySelector('.round-counter');
 const topLeft = document.querySelector('.top-left');
 const topRight = document.querySelector('.top-right');
 const bottomLeft = document.querySelector('.bottom-left');
