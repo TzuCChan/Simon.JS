c88d69893344e561b0d1188f6b8316993043dc58
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2023-10-30 12:59:58 -0400

    const score

--- game.js
+++ game.js
@@ -5,3 +5,4 @@ const topLeft = document.querySelector('.top-left');
 const topRight = document.querySelector('.top-right');
 const bottomLeft = document.querySelector('.bottom-left');
 const bottomRight = document.querySelector('.bottom-right');
+const score = document.querySelector('.score');
